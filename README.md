577e453b88e102174e10b63a966465420846a68e
Joona Tontti <joonatontti@hotmail.com>
2018-03-06 15:15:18 +0200

    jQuery functionality + related classes. Minor update to README.md
    Added list's functionality with jQuery and some related css classes. Minor update to README.md

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 My main purpose for this repo is to learn Git/Github.
 The repo will contain files of a browser To-Do list app.
 The app is a part of a web development bootcamp.
+It's coded with html, css and jQuery.
 Hope this project goes well and I learn many new things!
---
+++ assets/js/todos.js
@@ -0,0 +1,63 @@
+//Add click listeners to list items.
+// Listener strikethroughs the text by toggling "strikethrough" css class.
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("strikethrough");
+});
+
+//Alternative (longer) way of writing above:
+// $("li").click(function() {
+//     if($(this).css("color") === "rgb(128, 128, 128)") {
+//         $(this).css({
+//             textDecoration: "none",
+//             color: "black"
+//         });
+//     }
+//     else {
+//         $(this).css({
+//             textDecoration: "line-through",
+//             color: "gray"
+//         });
+//     }
+// }); //$("li").click()
+
+//Make "delete list item" buttons(spans) work.
+$("ul").on("click", "li span", function(event) {
+//Fade out the closest li (parent li in this case) of the "delete" button.
+//We could also use parent() function.
+    $(this).closest("li").fadeOut(500, function() {
+        //After fade out, delete the list item.
+        $(this).remove();
+    });
+    //Stop bubbling to prevent li click listener.
+    event.stopPropagation();
+})
+
+
+//Make text input field add new list item.
+$("input[name='newTask']").keypress(function(event) {
+    
+    //Save input value for a variable.
+    var inputValue = $(this).val();
+
+    //If user presses enter (which code = 13) and text field is not empty, add new list item.
+    if(event.which === 13 && inputValue !== "") {
+        //Add new list item with input field's text.
+        $("#container ul").append("<li><span>D</span>" + inputValue + "</li>");
+        //Empty the input text field.
+        $(this).val("");
+    }
+
+}); //$("input[name='newTask']").keypress()
+
+//Make hide button work.
+$("input[name='hide']").on("click", function(){
+    //Clicking the button toggles "hide" class for the input text field.
+    $("input[name='newTask']").toggleClass("hide");
+    //Change the button's icon accordingly.
+    if($(this).val() === " - ") {
+        $(this).val(" + ");
+    }
+    else {
+        $(this).val(" - ");
+    }
+});
